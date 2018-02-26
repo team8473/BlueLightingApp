9a09aee28765ec4bed7a88a7e7b70767c6642799
team8473 <gabriel.mahlen@egpg.org>
2018-02-26 09:51:21 -0600

    Testing path builder.

--- app/src/main/java/com/example/wahle/stuff/Path.java
+++ app/src/main/java/com/example/wahle/stuff/Path.java
@@ -1,20 +1,22 @@
 package com.example.wahle.stuff;
 
-import java.util.ArrayList;
-
 public class Path {
     private static final double ROBOT_WIDTH = 17.2;
-    private static final double WHEEL_DIAMETER = 4;
+    private static final double WHEEL_DIAMETER = 4.0;
     private static final double CIRCUMFRENCE = Math.PI * WHEEL_DIAMETER;
+    public static final double COUNTS_PER_MOTOR_REV = 374;
+    public static final double DRIVE_GEAR_REDUCTION = 2.0;
+    public static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER * 3.1415);
 
-    double leftDistance;
-    double rightDistance;
-    double leftSpeed;
-    double rightSpeed;
+    public static double leftDistance1, leftDistance2, leftDistance3;
+    public static double rightDistance1, rightDistance2, rightDistance3;
+    public double leftSpeed;
+    public double rightSpeed;
 
     public double convertDistance(double distance) {
         distance = Math.abs(distance);
-        return distance / CIRCUMFRENCE;
+        return distance * COUNTS_PER_INCH;
     }
 
     public void addLeftTurn(double angle, double radius, double speed) {
@@ -22,28 +24,40 @@ public class Path {
 
         double innerScale = innerScale(radius);
 
-        leftDistance = (distance * Math.abs(innerScale));
+        leftDistance3 = (distance * Math.abs(innerScale));
         leftSpeed = (speed * innerScale);
-        rightDistance = (distance);
+        rightDistance3 = (distance);
         rightSpeed = (speed);
+
     }
+
     public void addRightTurn(double angle, double radius, double speed) {
         double distance = convertDistance(angle, radius);
 
         double innerScale = innerScale(radius);
 
-        rightDistance = (distance * Math.abs(innerScale));
+        rightDistance2 = (distance * Math.abs(innerScale));
         rightSpeed = (speed * innerScale);
-        leftDistance = (distance);
+        leftDistance2 = (distance);
         leftSpeed = (speed);
 
+    }
+
+    public void addStraight(double distance, double speed) {
+        //distance = convertDistance(distance);
+
+        leftDistance1 = distance;
+        rightDistance1 = distance;
+        leftSpeed = speed;
+        rightSpeed = speed;
     }
 
     public double convertDistance(double angle, double radius) {
         angle *= Math.PI / 180.0; //convert to radians
         double distance = radius * angle;
-        return convertDistance(distance); // convert to revolutions
+        return distance; // convert to revolutions
     }
+
     public double innerScale(double radius) {
         radius = Math.abs(radius);
         double innerRadius = radius - ROBOT_WIDTH;
--- app/src/main/java/com/example/wahle/stuff/TestActivity.java
+++ app/src/main/java/com/example/wahle/stuff/TestActivity.java
@@ -9,18 +9,33 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import static com.example.wahle.stuff.Path.leftDistance1;
+import static com.example.wahle.stuff.Path.leftDistance2;
+import static com.example.wahle.stuff.Path.leftDistance3;
+import static com.example.wahle.stuff.Path.rightDistance1;
+import static com.example.wahle.stuff.Path.rightDistance2;
+import static com.example.wahle.stuff.Path.rightDistance3;
+
 public class TestActivity extends AppCompatActivity {
 
     TextView tv;
+    Path path = new Path();
+    String newXString, newYString, newRadius, newSpeed;
+    Path hi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        final EditText test = findViewById(R.id.Test);
+        final EditText x = findViewById(R.id.x);
+        final EditText y = findViewById(R.id.y);
+        final EditText r = findViewById(R.id.r);
+        final EditText s = findViewById(R.id.s);
         final Button b = findViewById(R.id.button);
-        tv= findViewById(R.id.textView3);
+        tv = findViewById(R.id.textView3);
         tv.setMovementMethod(new ScrollingMovementMethod());
 
         b.setOnClickListener(new View.OnClickListener() {
@@ -28,24 +43,37 @@ public class TestActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                final String str = test.getText().toString();
-                getSwitch(str);
+                String speedString = s.getText().toString();
+                Double newSpeed = Double.parseDouble(speedString);
+
+                String xString = x.getText().toString();
+                Double newXString = Double.parseDouble(xString);
+
+                String yString = y.getText().toString();
+                Double newYString = Double.parseDouble(yString);
+
+                String radiusString = r.getText().toString();
+                Double newRadius = Double.parseDouble(radiusString);
+
+                buildPath(newXString, newYString, newRadius, newSpeed);
+
+                tv.setText("Right Distance: " + rightDistance1 +
+                        "\n Left Disitance: " + leftDistance1  +
+                        "\n Right Distance: " + rightDistance2 +
+                        "\n Left Disitance: " + leftDistance2  +
+                        "\n Right Distance: " + rightDistance3 +
+                        "\n Left Disitance: " + leftDistance3);
 
             }
         });
     }
 
-    public String getSwitch(String str) {
-        switch (str) {
-            case "BLUE" :
-                tv.setText("BLUE");
-                break;
-            case "RED" :
-                tv.setText("RED");
-                break;
-            default:
-                tv.setText("\n HI");
-        }
-        return str;
+    public Path buildPath(Double x, Double y, Double r, Double s) {
+        ArrayList<PathBuilder.Waypoint> sWaypoints = new ArrayList();
+        sWaypoints.add(new PathBuilder.Waypoint(24,49,0,0));
+        sWaypoints.add(new PathBuilder.Waypoint(24,85,10,12));
+        sWaypoints.add(new PathBuilder.Waypoint(10,85,0,12));
+
+        return hi = PathBuilder.buildPathFromWaypoints(sWaypoints);
     }
 }
